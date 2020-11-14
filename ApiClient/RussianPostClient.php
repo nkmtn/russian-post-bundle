2287b0b540d15e5cc2d078441ec1391338aa6964
nkmtn <nkmtn10@gmail.com>
2020-11-14 15:28:59 +0300

    fix for any address

--- ApiClient/RussianPostClient.php
+++ ApiClient/RussianPostClient.php
@@ -47,7 +47,7 @@ class RussianPostClient
                 'AuthCode' => $this->accessToken,
             ],
             'json' => [
-                "addr" => [ ["val" => "респ. Северная Осетия - Алания, г. Владикавказ,   ул. Штыба, д. 2"] ],
+                "addr" => [ ["val" => $address] ],
                 "version" => "v7_2",
                 "reqId" => "12204cb4-37fb-4059-91e6-c6e17e946d7f"
             ]
